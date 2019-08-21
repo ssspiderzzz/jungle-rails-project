d732aedede7f7ab472d3f20a4df40f4d55c6f30a
ssspiderzzz <ssspiderzzz@gmail.com>
2019-08-21 20:33:56 +0000

    add products and categories number and style in dashbboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['USERNAME'], :password => ENV['PASSWORD']
 
   def show
+    @total_product_number = Product.all.count
+    @total_category_number = Category.all.count
   end
+  
 end
