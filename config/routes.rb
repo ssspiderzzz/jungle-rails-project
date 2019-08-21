a142a5234cc915a44a694f697fa392c5f1d1b2de
ssspiderzzz <ssspiderzzz@gmail.com>
2019-08-21 22:28:54 +0000

    add about route, only get, re-factorize the admin_categories path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :about, only: [:show]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -13,10 +12,12 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  match 'about' => 'about#show', :via => :get
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories
+    match 'categories' => 'categories#index', :via => :get
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
