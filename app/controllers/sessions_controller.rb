a8e946b18b38369def91e98b7660ab92650dc936
ssspiderzzz <ssspiderzzz@gmail.com>
2019-08-22 22:57:59 +0000

    add authorize method in carts, remove all the debug puts method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    puts params.inspect
     user = User.find_by_name(params[:name])
     # If the user exists AND the password entered is correct.
-    puts "user status: #{user.inspect}, user auth status: #{user.authenticate(params[:password]).inspect}"
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-      puts session[:user_id]
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    puts user_params.inspect
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
