10bf67da97d00e1d5e15db6d628b8d4f2da1805f
Haibin Zhu <ssspiderzzz@gmail.com>
2019-08-27 11:49:28 -0700

    add test for add cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
-
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,10 +16,12 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
     end
   end
 
-  scenario "It shows the product details" do
+  scenario "Add item will add 1 to My Cart number" do
     visit root_path
-
-    click_on('Details', match: :first )
-    expect(page).to have_css 'article.product-detail'
+    expect(page).to have_content 'My Cart (0)'
+    click_on('Add', match: :first )
+    # save_and_open_screenshot "test.png"
+    expect(page).to have_content 'My Cart (1)'
   end
 end
+  
